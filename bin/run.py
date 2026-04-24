52771774a18cc28e853f095f34b1ec068678634f
DavidMcGurk <david.mcgurk0@gmail.com>
2026-04-24 14:20:11 +0100

    Temporarily add dummy main script
    Signed-off-by: DavidMcGurk <david.mcgurk0@gmail.com>

--- bin/run.py
+++ bin/run.py
@@ -1,4 +1,4 @@
-from src.optimisation.predictor import Predictor
+# from src.optimisation.predictor import Predictor
 
 
 def main() -> None:
@@ -6,5 +6,6 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    predictor = Predictor()
-    predictor.load_player_data()
+    # predictor = Predictor()
+    # predictor.load_player_data()
+    main()
